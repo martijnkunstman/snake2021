982e17fcdd11ce0a359cd4e934bf2ec0e013720c
martijnkunstman <martijn@kunstman.nl>
2021-03-09 09:15:03 +0100

    les 2 - deel 1

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
-console.log("start2");
+let boardSize = 15;
+let board = document.getElementById("board");
+
+
+function drawBoard()
+{
+    for (let j = 0; j < boardSize; j++) {
+        for (let i = 0; i < boardSize; i++) {
+            board.innerHTML += "<div id='x" + i + "y"+j+"' class='cell'>" + i +"-"+ j+"</div>";
+        }
+        board.innerHTML += "<br>";
+    }
+}
+
+drawBoard();
+
+
+let halfBoardSize = Math.round(boardSize/2)-1;
+let snakeHeadPosition = "x"+halfBoardSize+"y"+halfBoardSize;
+console.log(snakeHeadPosition);
+let head = document.getElementById(snakeHeadPosition).style.background = "red";
