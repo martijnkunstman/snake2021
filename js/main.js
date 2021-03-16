764f04ae22a83d508f867b7f51e893b547de73c4
martijnkunstman <martijn@kunstman.nl>
2021-03-16 09:16:49 +0100

    Les 3: deel 1 (plaats voedsel)

--- js/main.js
+++ js/main.js
@@ -1,16 +1,18 @@
-let boardSize = 15;
+let boardSize = 13;
 let board = document.getElementById("board");
 let keyboardInput = document.getElementById("keyboardInput");
 //let direction = [0,0]; //alternatief
 let direction = 0;
-let position = { x: Math.floor(boardSize / 2), y: Math.floor(boardSize / 2) };
+let foodIsEaten = true;
+let snakePosition = { x: Math.floor(boardSize / 2), y: Math.floor(boardSize / 2) };
+let foodPosition = { x: 0, y: 0 };
 
 //drawBoard (maakt het speelveld dat is opgebouwd uit div elementen met een unieke id)
 
 function drawBoard() {
     for (let y = 0; y < boardSize; y++) {
         for (let x = 0; x < boardSize; x++) {
-            board.innerHTML += "<div id='x" + x + "y" + y + "' class='cell'>x"+x+"y"+y+"</div>";
+            board.innerHTML += "<div id='x" + x + "y" + y + "' class='cell'>x" + x + "y" + y + "</div>";
         }
         board.innerHTML += "<br>";
     }
@@ -29,30 +31,36 @@ function clearBoard() {
     */
 
     // bovenstaande kan met minder code door dit te doen....  
-    document.querySelectorAll('.cell').forEach(function(cell) {
+
+
+    document.querySelectorAll('.cell').forEach(function (cell) {
         cell.className = "cell";
-    }); 
+    });
+
+
+
 }
 
-//updatePosition
+//updatesnakePosition
 
-function updatePosition() {
-    //position.y += direction[1]; //alternatief
-    //position.x += direction[0]; //alternatief   
-  
+function updatesnakePosition() {
+
+    //snakePosition.y += direction[1]; //alternatief
+    //snakePosition.x += direction[0]; //alternatief   
+
     if (direction == 1) {
-        position.y = position.y - 1;
+        snakePosition.y = snakePosition.y - 1;
     }
     if (direction == 2) {
-        position.y = position.y + 1;
+        snakePosition.y = snakePosition.y + 1;
     }
     if (direction == 3) {
-        position.x = position.x + 1;
+        snakePosition.x = snakePosition.x + 1;
     }
     if (direction == 4) {
-        position.x = position.x - 1;
+        snakePosition.x = snakePosition.x - 1;
     }
-  
+
 }
 
 //resetGame
@@ -60,30 +68,47 @@ function updatePosition() {
 function resetGame() {
     //direction = [0,0]; //alternatief
     direction = 0;
-    position = { x: Math.floor((boardSize - 1) / 2), y: Math.floor((boardSize - 1) / 2) };
+    snakePosition = { x: Math.floor((boardSize - 1) / 2), y: Math.floor((boardSize - 1) / 2) };
 }
 
-//collisionCheck
+//collisionBoarderCheck
 
 function collisionCheck() {
-    if (position.x < 0 || position.y < 0 || position.x > boardSize - 1 || position.y > boardSize - 1) { resetGame() }
+    if (snakePosition.x < 0 || snakePosition.y < 0 || snakePosition.x > boardSize - 1 || snakePosition.y > boardSize - 1) { resetGame() }
 }
 
 //drawSnake
 
 function drawSnake() {
-    let snakeHeadPosition = "x" + position.x + "y" + position.y;
-    document.getElementById(snakeHeadPosition).className += " body";
+    let snakeHeadsnakePosition = "x" + snakePosition.x + "y" + snakePosition.y;
+    document.getElementById(snakeHeadsnakePosition).className += " bodySnake";
+}
+
+function drawFood() {
+    if (foodIsEaten) {
+        //
+        //todo: zorg er voor dat het voedsel nooit op de slang kan komen te staan!!
+        //
+        let xRandom = Math.floor(Math.random() * (boardSize - 1));
+        let yRandom = Math.floor(Math.random() * (boardSize f- 1));
+        foodPosition.x = xRandom;
+        foodPosition.y = yRandom;
+        foodIsEaten = false;
+    }
+    let foodPositionID = "x" + foodPosition.x + "y" + foodPosition.y;
+    document.getElementById(foodPositionID).className += " food";
 }
 
 //gameLoop
 
-function gameLoop()
-{
-    updatePosition();
+function gameLoop() {
+
+    updatesnakePosition();
     collisionCheck();
     clearBoard();
+    drawFood();
     drawSnake();
+
 }
 
 //start Game here......
@@ -98,7 +123,7 @@ setInterval(gameLoop, 500);
 // keyboard controls
 
 window.addEventListener("keydown", function (event) {
-    
+
     if (event.key == "ArrowUp") {
         //direction = [0,-1]; //alternatief
         direction = 1;
@@ -117,5 +142,5 @@ window.addEventListener("keydown", function (event) {
     }
 
     // todo: voeg WASD toe (voor de echte gamers onder ons....)
-    keyboardInput.innerHTML = "direction:"+direction;
+    keyboardInput.innerHTML = "direction:" + direction;
 }, true);
